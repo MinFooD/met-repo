bc544df3b9b761abda8a70f344869cc7f46dc4ed
NET14_PhucTran <phuctmse170256@fpt.edu.vn>
2024-11-24 14:50:05 +0700

    deploy

--- js/config.js
+++ js/config.js
@@ -1,4 +1,4 @@
 window.config = {
-  apiBaseUrl: 'localhost:7082', // URL khi chạy local
-  //   apiBaseUrl: 'met20241121211402.azurewebsites.net', // Hoặc URL khi deploy
+  // apiBaseUrl: 'localhost:7082', // URL khi chạy local
+  apiBaseUrl: 'met20241121211402.azurewebsites.net', // Hoặc URL khi deploy
 }
