e2c1968f2d7effebaa8925995b6524021998c149
NET14_PhucTran <phuctmse170256@fpt.edu.vn>
2024-11-24 20:16:54 +0700

    asd

--- build/index.bae9914b.js
+++
@@ -1,2 +0,0 @@
-document.addEventListener("DOMContentLoaded",()=>{let e=localStorage.getItem("logoutMessage");e&&(alert(e),localStorage.removeItem("logoutMessage"));let t=document.getElementById("CheckboxLarge"),o=document.getElementById("checkboxLabel");function n(){o.textContent=t.checked?"Writer":"Reader"}null!=t&&(t.addEventListener("change",n),n())});
-//# sourceMappingURL=index.bae9914b.js.map
