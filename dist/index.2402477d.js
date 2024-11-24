a96de29f4c9d4da3e1972d6a403c7f06c04405d4
NET14_PhucTran <phuctmse170256@fpt.edu.vn>
2024-11-24 18:23:40 +0700

    asd

---
+++ dist/index.2402477d.js
@@ -0,0 +1,2 @@
+const e=localStorage.getItem("jwtToken");e?console.log(e):window.location.href="/html/login.html",localStorage.getItem("loginName")&&(document.getElementById("loginName").textContent=`Hello ${localStorage.getItem("loginName")}`);
+//# sourceMappingURL=index.2402477d.js.map
--- dist/index.3b9c92a3.js
+++
@@ -1,2 +0,0 @@
-const e=localStorage.getItem("jwtToken");e?console.log(e):window.location.href="/login",localStorage.getItem("loginName")&&(document.getElementById("loginName").textContent=`Hello ${localStorage.getItem("loginName")}`);
-//# sourceMappingURL=index.3b9c92a3.js.map
